9bd17c7ed02455f7ef9ebaae6d57985e5ba87693
Nicolas <nicop4@users.noreply.github.com>
2020-12-10 08:48:19 +0100

    Build to create tag

--- include/version.h
+++ include/version.h
@@ -1,13 +1,13 @@
 //  DO NOT EDIT MANUALLY THIS FILE - IT IS DELETED AND RECREATED AT EACH BUILD !!!! 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "27"
+  #define BUILD_NUMBER "30"
 #endif
 #ifndef BUILD_HASH
-  #define BUILD_HASH "17e7eb"
+  #define BUILD_HASH "4071f6"
 #endif
 #ifndef BUILD_TIME
-  #define BUILD_TIME "2020-12-09 23:37:00.247171"
+  #define BUILD_TIME "2020-12-10 08:47:18.263456"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.17e7eb"
+  #define VERSION "v0.1.4071f6"
 #endif

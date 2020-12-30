8e6cd8853c880f5e14f5061d52952c75941174e7
Nicolas <nicop4@users.noreply.github.com>
2020-12-30 16:19:46 +0100

    remove build number

--- include/version.h
+++ include/version.h
@@ -1,12 +1,9 @@
 //  DO NOT EDIT MANUALLY THIS FILE - IT IS DELETED AND RECREATED AT EACH BUILD !!!! 
-#ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "52"
-#endif
 #ifndef BUILD_HASH
   #define BUILD_HASH "99bb01"
 #endif
 #ifndef BUILD_TIME
-  #define BUILD_TIME "2020-12-27 14:28:46.867219"
+  #define BUILD_TIME "2020-12-30 16:18:10.326431"
 #endif
 #ifndef VERSION
   #define VERSION "v0.2.99bb01"

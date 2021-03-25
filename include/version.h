e97a6173725981782b77055df63ba1b7a198d6c9
royto <royto81+github@gmail.com>
2021-03-25 21:29:14 +0100

    bump version

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 //  DO NOT EDIT MANUALLY THIS FILE - IT IS DELETED AND RECREATED AT EACH BUILD !!!! 
 #ifndef BUILD_HASH
-  #define BUILD_HASH "d7aaa1"
+  #define BUILD_HASH "003bc4"
 #endif
 #ifndef BUILD_TIME
-  #define BUILD_TIME "2021-03-23 23:25:27.983382"
+  #define BUILD_TIME "2021-03-25 21:29:00.873055"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.3.d7aaa1"
+  #define VERSION "v0.3.003bc4"
 #endif

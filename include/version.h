4616b1b5658e9f886a7915b815f2807a5207e9d6
Nicolas <nicop4@users.noreply.github.com>
2021-05-14 12:20:31 +0200

    Bump version & tag

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 //  DO NOT EDIT MANUALLY THIS FILE - IT IS DELETED AND RECREATED AT EACH BUILD !!!! 
 #ifndef BUILD_HASH
-  #define BUILD_HASH "003bc4"
+  #define BUILD_HASH "df7b2a"
 #endif
 #ifndef BUILD_TIME
-  #define BUILD_TIME "2021-03-25 21:29:00.873055"
+  #define BUILD_TIME "2021-05-14 12:20:00.873558"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.3.003bc4"
+  #define VERSION "v0.3.df7b2a"
 #endif

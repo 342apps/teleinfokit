e3b146fd2c35fa7d9735bb355dba76276056973b
Nicolas <nicop4@users.noreply.github.com>
2021-11-06 18:47:52 +0100

    Version 1.0.0

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 //  DO NOT EDIT MANUALLY THIS FILE - IT IS DELETED AND RECREATED AT EACH BUILD !!!! 
 #ifndef BUILD_HASH
-  #define BUILD_HASH "1a96a3"
+  #define BUILD_HASH "6bbff8"
 #endif
 #ifndef BUILD_TIME
-  #define BUILD_TIME "2021-11-06 10:36:10.759097"
+  #define BUILD_TIME "2021-11-06 18:42:52.323661"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.0.0.1a96a3-beta1"
+  #define VERSION "v1.0.0.6bbff8"
 #endif

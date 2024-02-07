1aeead6d94b08f4355c79af191afc8c0fded1c7d
Nicolas <nicop4@users.noreply.github.com>
2024-02-07 23:38:23 +0100

    Update libs

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
 //  DO NOT EDIT MANUALLY THIS FILE - IT IS DELETED AND RECREATED AT EACH BUILD !!!! 
 #ifndef BUILD_HASH
-  #define BUILD_HASH "775dcd"
+  #define BUILD_HASH "d9e047"
 #endif
 #ifndef VERSION
-  #define VERSION "v2.0.0.775dcd"
+  #define VERSION "v2.0.0.d9e047"
 #endif

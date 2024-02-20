b701cc3a441eec1913ef8e7724f27ded0f70aa03
Nicolas <nicop4@users.noreply.github.com>
2024-02-21 00:11:25 +0100

    Script to get chip id runs
    but need restart of chip to upload

--- pre_upload.py
+++ pre_upload.py
@@ -1,24 +1,41 @@
+Import("env")
+
 import glob
 import re
 from serial.tools.list_ports import comports
+import subprocess
 
 # Fonction exécutée avant l'upload
 def before_upload(source, target, env):
     print("Lecture du Chip ID...")
 
-    output = env.GetCommandOutput(f"esptool.py --chip esp8266 --port /dev/cu.usbserial* chip_id")
+    try:
+        # print("Forcer le reset de l'ESP32...")
+        # subprocess.run("esptool.py --chip esp8266 --port /dev/cu.usbserial* --before no_reset erase_flash")
 
-    # Utilise des expressions régulières pour extraire MAC et Chip ID
-    mac_match = re.search(r"MAC:\s+([0-9a-fA-F:]+)", output)
-    chip_id_match = re.search(r"Chip ID:\s+0x([0-9a-fA-F]+)", output)
-
-    if mac_match and chip_id_match:
-        mac_address = mac_match.group(1)
-        chip_id = chip_id_match.group(1)
         
-        print(f"MAC Address: {mac_address}")
-        print(f"Chip ID: {chip_id}")
-    else:
-        print("Impossible d'extraire les informations. Veuillez vérifier la sortie de la commande.")
+        output = subprocess.check_output(
+            f"esptool.py --chip esp8266 --port /dev/cu.usbserial* chip_id", 
+            shell=True,
+            text=True
+        )
+
+        # Utilise des expressions régulières pour extraire MAC et Chip ID
+        mac_match = re.search(r"MAC:\s+([0-9a-fA-F:]+)", output)
+        chip_id_match = re.search(r"Chip ID:\s+0x([0-9a-fA-F]+)", output)
+
+        if mac_match and chip_id_match:
+            mac_address = mac_match.group(1)
+            chip_id = chip_id_match.group(1)
+            
+            print(f"====> MAC Address: {mac_address}")
+            print(f"====> Chip ID: {chip_id}")
+        else:
+            print("Impossible d'extraire les informations. Veuillez vérifier la sortie de la commande.")
+
+    except subprocess.CalledProcessError as e:
+            print(f"Erreur lors de l'exécution de la commande esptool.py : {e}")
 
 
+
+env.AddPreAction("upload", before_upload)

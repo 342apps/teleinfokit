4af279e0f6f1217816d19db5a536c6428c1aef57
Damiendhn <damiendhn@gmail.com>
2026-02-06 12:56:24 +0100

    Update version

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
 //  DO NOT EDIT MANUALLY THIS FILE - IT IS DELETED AND RECREATED AT EACH BUILD !!!! 
 #ifndef BUILD_HASH
-  #define BUILD_HASH "390806"
+  #define BUILD_HASH "de6476"
 #endif
 #ifndef VERSION
-  #define VERSION "v2.1.0.390806"
+  #define VERSION "v2.1.0.de6476"
 #endif
